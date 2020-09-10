9dcfc087efb6a9ae2e1e77b5b31d70ad28d018e0
Eryk Fryderyk <grzeniu098@gmail.com>
2020-09-10 22:40:26 +0200

    add simple engine game code

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,87 @@
-const columns = document.querySelectorAll('.column-box');
+// const columns = document.querySelectorAll('.column-box');
 
 
 
-columns.forEach(col => {
-    col.addEventListener('click', () =>{
-        if (col.firstElementChild.classList.contains('block--active')){
-            col.firstElementChild.classList.remove('block--active');
-        }else {
-            col.firstElementChild.classList.add('block--active');
-        }
+// columns.forEach(col => {
+//     col.addEventListener('click', () =>{
+//         if (col.firstElementChild.classList.contains('block--active')){
+//             col.firstElementChild.classList.remove('block--active');
+//         }else {
+//             col.firstElementChild.classList.add('block--active');
+//         }
+//     })
+// })
+
+
+let activeBlock = null;
+// const numberOfAllBlocks = $blocks.length;
+
+
+const main = () => {
+    prepareDOMElements();
+    prepareDOMEvents();
+
+}
+
+const prepareDOMElements = () => {
+    $gameBoard = document.querySelector('.game-board');
+    $blocks = document.querySelectorAll('.block');
+    $lastColumn = document.querySelector('column-box__last-element')
+
+};
+
+const prepareDOMEvents = () => {
+    $gameBoard.addEventListener('click', e => {
+        selectBlock(e);
     })
-})
+};
+
+
+const selectBlock = e => {
+
+    const target = e.target;  
+    console.log(target);
+    if(activeBlock === null) {
+        if(target.children.length === 0){
+            return;
+        }else {
+            target.firstElementChild.classList.add('block--active');
+            activeBlock = target.firstElementChild
+        }
+    } else {
+        moveBlock(e);
+    }
+}
+
+const moveBlock = e => {
+    const target = e.target;
+    if(target.classList.contains('column-box')){
+        if(target.children.length === 0) {
+            createNewBlock(target);
+        } else if(activeBlock.id < target.firstElementChild.id){
+            createNewBlock(target);        
+        }
+    } else {
+        return;
+    }
+    activeBlock.classList.remove('block--active');
+    activeBlock = null;
+    activeBlock();
+    checkWin();
+}
+
+const createNewBlock = (target) => {
+    activeBlock.classList.remove('block--active');
+    const div = document.createElement('div');
+    div.classList = activeBlock.classList;
+    div.innerText = activeBlock.textContent;
+    div.id = activeBlock.id;
+    
+
+    div.classList.remove('active');
+    target.prepend(div);
+    activeBlock.remove();
+}
+
+
+document.addEventListener('DOMContentLoaded', main);
