1f55f93fc0282953c16521a59b0671ce4e42e84b
Eryk Fryderyk <grzeniu098@gmail.com>
2020-09-16 22:55:57 +0200

    update style file

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,3 @@
-// const columns = document.querySelectorAll('.column-box');
-
-
-
-// columns.forEach(col => {
-//     col.addEventListener('click', () =>{
-//         if (col.firstElementChild.classList.contains('block--active')){
-//             col.firstElementChild.classList.remove('block--active');
-//         }else {
-//             col.firstElementChild.classList.add('block--active');
-//         }
-//     })
-// })
 
 const $blocks = document.querySelectorAll('.block');
 console.log($blocks.length);
